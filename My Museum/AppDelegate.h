d83ce76e20b08994c5395399a790b85f0f6324af
ahsyuyu <ahsyuyu@gmail.com>
2015-01-16 14:36:55 +0800

    initialize

---
+++ My Museum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My Museum
+//
+//  Created by Anne on 2015/1/16.
+//  Copyright (c) 2015年 Anne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ My Museum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My Museum
+//
+//  Created by Anne on 2015/1/16.
+//  Copyright (c) 2015年 Anne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
