fac7d7a28510d1ec27ce25a50a9ced76e9d557b8
ahsyuyu <ahsyuyu@gmail.com>
2015-01-18 14:18:59 +0800

    adjust image in viewController

--- My Museum/ViewController.m
+++ My Museum/ViewController.m
@@ -19,16 +19,19 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    //[self.view setBackgroundColor:[UIColor grayColor]];
-    UIImageView *myImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 50, 400, 400)];
+    [self.view setBackgroundColor:[UIColor grayColor]];
+    UIImageView *myImageView = [[UIImageView alloc] initWithFrame:CGRectMake(15, 50, 350, 350)];
     myImageView.image = [UIImage imageNamed:@"white.png"];
+    myImageView.clipsToBounds = YES;
+    myImageView.layer.cornerRadius = 175;
+    myImageView.layer.borderColor = [UIColor whiteColor].CGColor;
+    myImageView.layer.borderWidth = 1.0;
     
-    myImageView.layer.borderColor = [UIColor blackColor].CGColor;
-    
+
     UITextView *myTextView = [[UITextView alloc] initWithFrame:CGRectMake(0, 450, 350, 300)];
-//    myTextView.backgroundColor = [UIColor blackColor];
+    myTextView.backgroundColor = [UIColor grayColor];
     myTextView.text = @"「清　翠玉白菜」是中華民國在臺灣的國立故宮博物院所珍藏的玉器雕刻，長18.7公分，寬9.1公分，厚5.07公分，利用翠玉天然的色澤雕出白菜的形狀。";
-//    myTextView.textColor = [UIColor whiteColor];
+    myTextView.textColor = [UIColor whiteColor];
     myTextView.font = [UIFont systemFontOfSize:20];
     
     
